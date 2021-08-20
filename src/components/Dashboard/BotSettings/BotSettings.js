86519976bcc93f1670a531577441c843d0933e29
ibrokhimov06 <ibrokhimov06@gmail.com>
2021-08-20 18:52:54 +0500

    bot error handler

--- src/components/Dashboard/BotSettings/BotSettings.js
+++ src/components/Dashboard/BotSettings/BotSettings.js
@@ -2,8 +2,8 @@ import React from "react";
 import { getBots, deleteBot, createBot } from "../../../api/calls/bot-settings";
 import TableCRUD from "../../Reusable/TableCRUD";
 import BotSettingsForm from "./BotSettingsForm";
-const headerCols = ["Имя", "Имя пользователя"];
-const bodyCols = ["first_name", "username"];
+const headerCols = ["Имя", "Имя пользователя", "Токен"];
+const bodyCols = ["first_name", "username", "token"];
 
 const BotSettings = ({}) => {
   return (
