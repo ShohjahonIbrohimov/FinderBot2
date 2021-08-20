17a216f2d3591b34c8d34be80c182c9a1522274e
ibrokhimov06 <ibrokhimov06@gmail.com>
2021-08-20 18:55:35 +0500

    bot error handler

--- src/components/Reusable/Table.js
+++ src/components/Reusable/Table.js
@@ -12,9 +12,13 @@ const Table = ({ tableData, headerCols, bodyCols, handleDelete }) => {
       <tbody>
         {tableData.map((p) => (
           <tr>
-            {bodyCols.map((col) => (
-              <td>{p[col]}</td>
-            ))}
+            {bodyCols.map((col) => {
+              if (col === "username") {
+                return <td>@{p[col]}</td>;
+              } else {
+                return <td>{p[col]}</td>;
+              }
+            })}
             <td style={{ display: "flex", justifyContent: "space-around" }}>
               <span class="icon_wrap" onClick={() => handleDelete(p._id)}>
                 <i class="bx bxs-trash-alt" style={{ color: "#555" }}></i>
