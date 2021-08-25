39fa3751e69e47dccc80f7b20aaa4458c79d0e94
ibrokhimov06 <ibrokhimov06@gmail.com>
2021-08-25 19:29:57 +0500

    as

---
+++ src/components/Dashboard/News/AddNews/ImportantNewsForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import LoaderBtn from "../../../Reusable/LoaderBtn";
+
+const MenuForm = ({ handleSubmit, onSubmit, register, loading }) => {
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-inputs-wrap">
+        <label>Название меню:</label>
+        <br />
+        <textarea
+          rows="8"
+          type="tgaccount"
+          name="name"
+          ref={register({ required: true })}
+        />
+        <br />
+      </div>
+      <LoaderBtn loading={loading} title="Добавить" />
+    </form>
+  );
+};
+
+export default MenuForm;
--- src/components/Dashboard/News/index.js
+++ src/components/Dashboard/News/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import CreateNews from "./AddNews/CreateNews";
 
 const index = () => {
-  return <div>{/* <CreateNews /> */}</div>;
+  return (
+    <div>
+      <CreateNews />
+    </div>
+  );
 };
 
 export default index;
