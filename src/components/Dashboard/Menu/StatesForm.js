664a640fc4f5cfc734066f9b6948eb4686c54039
ibrokhimov06 <ibrokhimov06@gmail.com>
2021-08-25 15:23:17 +0500

    category_id and name add state

--- src/components/Dashboard/Menu/StatesForm.js
+++ src/components/Dashboard/Menu/StatesForm.js
@@ -20,19 +20,14 @@ const ProductsForm = ({
     getCategories().then((res) => setcategories(res.data.data));
   }, [change]);
 
-  const podcat_id = watch("podcategory_id");
   const cat_id = watch("category_id");
 
   useEffect(() => {
     setValue(
-      "podcategory_name",
-      podcategories.filter((c) => c._id === podcat_id)[0]?.name
-    );
-    setValue(
       "category_name",
-      categories.filter((c) => c._id === cat_id)[0]?.name
+      podcategories.filter((c) => c._id === cat_id)[0]?.name
     );
-  }, [podcat_id, cat_id]);
+  }, [cat_id]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="form">
@@ -43,31 +38,12 @@ const ProductsForm = ({
         <br />
         <label>Название подкатегории:</label>
         <br />
-        <select name="podcategory_id" ref={register({ required: true })}>
+        <select name="category_id" ref={register({ required: true })}>
           {podcategories.map((c) => (
             <option value={c._id}>{c.name}</option>
           ))}
         </select>
         <br />
-        <label>Название категории:</label>
-        <br />
-        <select name="category_id" ref={register({ required: true })}>
-          {categories.map((c) => (
-            <option value={c._id}>{c.name}</option>
-          ))}
-        </select>
-        <br />
-        <input
-          style={{
-            height: "0px",
-            margin: "0px",
-            padding: "0px",
-            overflow: "hidden",
-            border: "none",
-          }}
-          name="podcategory_name"
-          ref={register({ required: true })}
-        />
         <input
           style={{
             height: "0px",
